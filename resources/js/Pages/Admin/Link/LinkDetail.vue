c01473c3e7f4e160464de03d92f377d9f72eec75
TranThanhQuan <quanlive123@gmail.com>
2025-04-14 16:17:15 +0700

    update qrcode shorten link

--- resources/js/Pages/Admin/Link/LinkDetail.vue
+++ resources/js/Pages/Admin/Link/LinkDetail.vue
@@ -3,37 +3,57 @@
 
 
         <!-- link info -->
-        <div class="my-5 p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tiêu đề link: {{ link.title }}</h5>
-            <h4 class="text-2xl dark:text-white">
-                Link gốc: <a :href="link.original_url" class="text-blue-500" target="_blank" rel="noopener noreferrer">
-                            {{ link.original_url }}
-                            </a>
-            </h4>
+        <div
+            class="my-5 p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 flex items-center justify-between">
 
-            <h4 class="font-normal text-gray-700 dark:text-gray-400">
-                Link rút gọn: <a class="text-blue-500 " target="_blank" rel="noopener noreferrer" :href="`https://${domain}/${link.short_code}`">
-                                        https://{{ domain }}/{{ link.short_code }}
-                                    </a>
-            </h4>
+            <div class="">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tiêu đề: {{
+                    link.title }}</h5>
+                <h4 class="text-2xl dark:text-white">
+                    Link gốc: <a :href="link.original_url" class="text-blue-500" target="_blank"
+                        rel="noopener noreferrer">
+                        {{ link.original_url }}
+                    </a>
+                </h4>
 
+                <h4 class="font-normal text-gray-700 dark:text-gray-400">
+                    Link rút gọn: <a class="text-blue-500 " target="_blank" rel="noopener noreferrer"
+                        :href="`https://${domain}/${link.short_code}`">
+                        https://{{ domain }}/{{ link.short_code }}
+                    </a>
+                </h4>
+
+                <h4 class="font-normal text-gray-700 dark:text-gray-400">Thời gian tồn tại: 23 Ngày 2 giờ</h4>
+                <h4 class="font-normal text-gray-700 dark:text-gray-400">Trạng thái: Công khai</h4>
+
+            </div>
+
+            <div>
+                <img :src="qrcode" alt="QR Code" />
+            </div>
         </div>
         <!-- end link info -->
 
         <!-- card list -->
         <div class="my-5 flex items-center justify-between">
-            <div class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 text-center dark:text-white">Tổng lượt truy cập</h5>
+            <div
+                class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 text-center dark:text-white">Tổng lượt
+                    truy cập</h5>
                 <p class="text-xl text-gray-700 text-center dark:text-gray-400">10</p>
             </div>
 
-            <div class="w-full mx-5 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 text-center dark:text-white">Top hệ điều hành</h5>
+            <div
+                class="w-full mx-5 p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 text-center dark:text-white">Top hệ điều
+                    hành</h5>
                 <p class="text-xl text-gray-700 text-center dark:text-gray-400">Ios</p>
             </div>
 
-            <div class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 text-center dark:text-white">Quốc gia hàng đầu</h5>
+            <div
+                class="w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 text-center dark:text-white">Quốc gia
+                    hàng đầu</h5>
                 <p class="text-xl text-gray-700 text-center dark:text-gray-400">Việt Nam</p>
             </div>
         </div>
@@ -46,25 +66,37 @@
         <div class="w-full bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
             <div class="sm:hidden">
                 <label for="tabs" class="sr-only">Select tab</label>
-                <select id="tabs" class="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 text-sm rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select id="tabs"
+                    class="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 text-sm rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option>Lượt truy cập</option>
                     <option>Hệ điều hành</option>
                     <option>Quốc gia</option>
                 </select>
             </div>
-            <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400 rtl:divide-x-reverse" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
+            <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400 rtl:divide-x-reverse"
+                id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
                 <li class="w-full">
-                    <button id="clicks-tab" data-tabs-target="#clicks" type="button" role="tab" aria-controls="clicks" aria-selected="true" class="inline-block w-full p-4 rounded-ss-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Lượt truy cập</button>
+                    <button id="clicks-tab" data-tabs-target="#clicks" type="button" role="tab" aria-controls="clicks"
+                        aria-selected="true"
+                        class="inline-block w-full p-4 rounded-ss-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Lượt
+                        truy cập</button>
                 </li>
                 <li class="w-full">
-                    <button id="operating-system-tab" data-tabs-target="#operating-system" type="button" role="tab" aria-controls="operating-system" aria-selected="false" class="inline-block w-full p-4 bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Hệ điều hành</button>
+                    <button id="operating-system-tab" data-tabs-target="#operating-system" type="button" role="tab"
+                        aria-controls="operating-system" aria-selected="false"
+                        class="inline-block w-full p-4 bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Hệ
+                        điều hành</button>
                 </li>
                 <li class="w-full">
-                    <button id="nation-tab" data-tabs-target="#nation" type="button" role="tab" aria-controls="nation" aria-selected="false" class="inline-block w-full p-4 rounded-se-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Quốc gia</button>
+                    <button id="nation-tab" data-tabs-target="#nation" type="button" role="tab" aria-controls="nation"
+                        aria-selected="false"
+                        class="inline-block w-full p-4 rounded-se-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600">Quốc
+                        gia</button>
                 </li>
             </ul>
             <div id="fullWidthTabContent" class="border-t border-gray-200 dark:border-gray-600">
-                <div class="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="clicks" role="tabpanel" aria-labelledby="clicks-tab">
+                <div class="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="clicks" role="tabpanel"
+                    aria-labelledby="clicks-tab">
                     <!-- chart clicks-->
                     <div v-apex-tooltip-style class="w-full mx-auto bg-white rounded-lg shadow dark:bg-gray-800">
                         <div class="flex items-center justify-between mb-4">
@@ -72,36 +104,31 @@
                         </div>
                         <div class="flex items-center justify-between mb-4">
                             <!-- date picker -->
-                            <VueDatePicker v-model="date" range multi-calendars :preset-dates="presetDates" auto-apply locale="vi" :format="format">VueDatePicker>
+                            <VueDatePicker v-model="date" range multi-calendars :preset-dates="presetDates" auto-apply
+                                locale="vi" :format="format">VueDatePicker>
                                 <template #preset-date-range-button="{ label, value, presetDate }">
-                                    <span
-                                        role="button"
-                                        :tabindex="0"
-                                        @click="presetDate(value)"
+                                    <span role="button" :tabindex="0" @click="presetDate(value)"
                                         @keyup.enter.prevent="presetDate(value)"
                                         @keyup.space.prevent="presetDate(value)">
-                                    {{ label }}
+                                        {{ label }}
                                     </span>
                                 </template>
                             </VueDatePicker>
 
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Gửi</button>
+                            <button type="button"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Gửi</button>
                         </div>
 
 
 
                         <div class="h-[300px]">
-                            <apexchart
-                                height="100%"
-                                type="bar"
-                                :options="chartOptionsBar"
-                                :series="seriesBar"
-                                />
+                            <apexchart height="100%" type="bar" :options="chartOptionsBar" :series="seriesBar" />
                         </div>
                     </div>
                 </div>
 
-                <div class="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="operating-system" role="tabpanel" aria-labelledby="operating-system-tab">
+                <div class="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="operating-system"
+                    role="tabpanel" aria-labelledby="operating-system-tab">
                     <!-- chart opereting system -->
                     <div v-apex-tooltip-style class="w-full mx-auto bg-white rounded-lg shadow dark:bg-gray-800">
                         <div class="flex items-center justify-between mb-4">
@@ -109,65 +136,54 @@
                         </div>
                         <div class="flex items-center justify-between mb-4">
                             <!-- date picker -->
-                            <VueDatePicker v-model="date" range multi-calendars :preset-dates="presetDates" auto-apply locale="vi" :format="format">
+                            <VueDatePicker v-model="date" range multi-calendars :preset-dates="presetDates" auto-apply
+                                locale="vi" :format="format">
                                 <template #preset-date-range-button="{ label, value, presetDate }">
-                                    <span
-                                        role="button"
-                                        :tabindex="0"
-                                        @click="presetDate(value)"
+                                    <span role="button" :tabindex="0" @click="presetDate(value)"
                                         @keyup.enter.prevent="presetDate(value)"
                                         @keyup.space.prevent="presetDate(value)">
-                                    {{ label }}
+                                        {{ label }}
                                     </span>
                                 </template>
                             </VueDatePicker>
 
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Gửi</button>
+                            <button type="button"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Gửi</button>
                         </div>
 
                         <div class="h-[300px]">
-                            <apexchart
-                                height="100%"
-                                type="area"
-                                :options="chartOptions"
-                                :series="series"
-                            />
+                            <apexchart height="100%" type="area" :options="chartOptions" :series="series" />
                         </div>
                     </div>
                 </div>
 
-                <div class="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="nation" role="tabpanel" aria-labelledby="nation-tab">
-                     <!-- chart nation -->
+                <div class="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="nation" role="tabpanel"
+                    aria-labelledby="nation-tab">
+                    <!-- chart nation -->
                     <div v-apex-tooltip-style class="w-full mx-auto bg-white rounded-lg shadow dark:bg-gray-800">
                         <div class="flex items-center justify-between mb-4">
                             <h2 class="text-xl font-semibold text-gray-800 dark:text-white">Thống kê quốc gia</h2>
                         </div>
                         <div class="flex items-center justify-between mb-4">
                             <!-- date picker -->
-                            <VueDatePicker v-model="date" range multi-calendars :preset-dates="presetDates" auto-apply locale="vi" :format="format" >
+                            <VueDatePicker v-model="date" range multi-calendars :preset-dates="presetDates" auto-apply
+                                locale="vi" :format="format">
                                 <template #preset-date-range-button="{ label, value, presetDate }">
-                                    <span
-                                        role="button"
-                                        :tabindex="0"
-                                        @click="presetDate(value)"
+                                    <span role="button" :tabindex="0" @click="presetDate(value)"
                                         @keyup.enter.prevent="presetDate(value)"
                                         @keyup.space.prevent="presetDate(value)">
-                                    {{ label }}
+                                        {{ label }}
                                     </span>
                                 </template>
                             </VueDatePicker>
 
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Gửi</button>
+                            <button type="button"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Gửi</button>
                         </div>
 
                         <div class="h-[300px]">
                             <!-- Pie Chart -->
-                                <apexchart
-                                height="300"
-                                type="pie"
-                                :options="chartOptionsPie"
-                                :series="seriesPie"
-                                />
+                            <apexchart height="300" type="pie" :options="chartOptionsPie" :series="seriesPie" />
                         </div>
                     </div>
                 </div>
@@ -183,8 +199,12 @@
 import AdminLayout from '../Components/AdminLayout.vue'
 import { ref, onMounted, watch } from 'vue';
 import { Link, router } from '@inertiajs/vue3';
-
 import { endOfMonth, endOfYear, startOfMonth, startOfYear, subMonths } from 'date-fns';
+import { useQRCode } from '@vueuse/integrations/useQRCode'
+import { shallowRef } from 'vue';
+
+
+
 
 const date = ref();
 
@@ -224,10 +244,14 @@ const format = (dates) => {
 };
 
 const props = defineProps({
-  link:Object
+  link: Object
 });
 
 const domain = ref(window.location.hostname);
+
+// QR code
+const qrcode = useQRCode(`https://${domain.value}/${props.link.short_code}`);
+
 
 
 
@@ -362,6 +386,26 @@ const seriesBar = ref([
     data: [120, 200, 150, 300, 123,242 , 122]
   }
 ]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </script>
 
 <style scoped>
